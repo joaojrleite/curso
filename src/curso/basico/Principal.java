e18096af6790cb243d18bc34cb19b5b1f7526602
João Leite Jr. <joaojrleite@gmail.com>
2023-05-18 16:33:53 -0300

    Alterei a mensagem

--- src/curso/basico/Principal.java
+++ src/curso/basico/Principal.java
@@ -2,8 +2,10 @@ package curso.basico;
 
 public class Principal {
 	
+	private String nome;
+	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello, World!");
 	}
 
 }
